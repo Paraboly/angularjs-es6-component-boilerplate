0233b39a18799dc5134053bc2609f547bf03cfba
Haldun Yildiz <haldun313@gmail.com>
2020-02-15 16:00:29 +0300

    Move IndexJS to lib folder

--- gulp-paths.const.js
+++ gulp-paths.const.js
@@ -16,7 +16,7 @@ const paths = {
   js: resolveToComponents("**/*!(.spec.js).js"), // exclude spec files
   scss: resolveToApp("**/*.scss"), // stylesheets
   html: [resolveToApp("**/*.html"), path.join(root, "index.html")],
-  entry: ["babel-polyfill", path.join(__dirname, root, "../index.js")],
+  entry: ["babel-polyfill", path.join(__dirname, root, "index.js")],
   output: root,
   blankTemplates: path.join(__dirname, "generator", "component/**/*.**"),
   dest: path.join(__dirname, "dist")
